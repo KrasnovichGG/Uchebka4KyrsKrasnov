46a6e075ac4cc70a0bba27eb7ce7e089ebc0badc
Alexsunder <Sasha-kr90@bk.ru>
2022-11-10 14:18:04 +0300

    Добавьте файлы проекта.

---
+++ Uchebka4KyrsKrasnov/RegAndLog/WinReg.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Uchebka4KyrsKrasnov.RegAndLog
+{
+    /// <summary>
+    /// Логика взаимодействия для WinReg.xaml
+    /// </summary>
+    public partial class WinReg : Window
+    {
+        public WinReg()
+        {
+            InitializeComponent();
+        }
+    }
+}
